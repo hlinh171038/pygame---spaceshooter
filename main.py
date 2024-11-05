b74513fe98fb039ca95ef2f4168e30a1ffcb2290
HL <hoanglinh171038@gmail.com>
2024-11-05 07:44:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+# upgrade system help you find the path of image
+import os
+
+WIDTH, HEIGHT = 900, 500 # CONTANT
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # create frame with WIDTH and HEIGHT
+WHITE = (255,255,255)
+# 3 .fps
+FPS = 60
+# 5 sacle up
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+# 4. create character
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+
+#scale for character red
+# 6 rotate
+RED_SPACESHIP =pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90) 
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+#scale for character yellow
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+# 1.change caption 
+pygame.display.set_caption("first game !")
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(RED_SPACESHIP, (300,100))
+    WIN.blit(YELLOW_SPACESHIP, (700, 100))
+    pygame.display.update() 
+    
+
+
+def main():
+
+    #7 upgrade the draw_window function
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        # 1. change color bg
+            draw_window()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
